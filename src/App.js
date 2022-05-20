c80e4b8edbd599bddc46aadf526ddb5cf655123c
unknown <tareqrobin57@gmail.com>
2022-05-20 12:59:10 +0600

    requireAuth emplemented on product page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Js/Header';
 import Home from './Components/Js/Home/Home';
 import Products from './Components/Js/Products';
 import Login from './Components/Js/Security/Login';
+import RequireAuth from './Components/Js/Security/RequireAuth';
 import Signup from './Components/Js/Security/Signup';
 
 function App() {
@@ -14,7 +15,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/products' element={
+          <RequireAuth>
+            <Products></Products>
+          </RequireAuth>}>
+        </Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
